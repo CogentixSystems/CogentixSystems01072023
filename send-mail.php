f628bceda24ee248d8b4cc825cc8a6ad9b71b9ee
CogentixSystems <137160492+CogentixSystems@users.noreply.github.com>
2023-07-04 14:05:34 +0530

    done

--- send-mail.php
+++ send-mail.php
@@ -8,13 +8,13 @@ $message = $_POST['message'];
 
 $mailheader = "From:".$name."<".$email.">\r\n";
 
-$to = "bio@rishitomar.in";
+$to = "naveen03071982@gmail.com";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Phone No.  =" . $phone ."\r\n  Message = ". $message;
 
 if ($email and $name and $subject and $message)
 {
     mail($to, $subject, $txt, $mailheader);
-    echo "<script type='text/javascript'>alert('mail sent successfully!') </script>";
+    echo "<script type='text/javascript'> window.location.href = 'index.html' </script>";
 }
 else
 {
